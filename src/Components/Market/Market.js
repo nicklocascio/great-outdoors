9bd7b6fb170bfebb6bc0d987a70f41d0efc7563f
Nicholas Locascio <nicklocascio@rainmaker.dhcp.nd.edu>
2022-03-03 16:32:54 -0500

    services set up ready to link to front end

--- src/Components/Market/Market.js
+++ src/Components/Market/Market.js
@@ -1,24 +1,15 @@
 import MarketList from "./MarketList";
+
+
 import React, {useState} from 'react';
 import {useEffect} from 'react';;
 
-import { getAllMarket } from "../../Services/getAllMarket";
-import { getItemById } from "../../Services/getItem";
-import { getReviewById } from "../../Services/getReview";
-import { getAllReviews } from "../../Services/getAllReviews";
 
 //Instead of doing the data read in App, we did it in Market
 //to simulate what we will do in our project
 const Market = () => {
 
-    const [items, setItems] = useState();
-
-    useEffect(() => {
-        console.log("in use effect...");
-        getAllMarket().then((data) => {
-            setItems(data);
-        });
-    }, []);
+    
 
     return(
         <div></div>
